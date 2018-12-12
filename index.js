bc7825c8a8f220bdf4df8ab656292c0d4c84b27c
jakasco <jakasco@users.noreply.github.com>
2018-12-12 11:38:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 'use strict';
 require('dotenv').config();
-const express = require('express');
-// const fs      = require('fs');
-// const https   = require('https');
 
+const express = require('express');
 const database = require('./modules/database');
 const resize = require('./modules/resize');
 const exif = require('./modules/exif');
 const multer = require('multer');
 const upload = multer({dest: 'public/files/'});
-
 const app = express();
-
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/*
-const sslkey  = fs.readFileSync('/etc/pki/tls/private/ca.key');
-const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
-const options = {
-  key: sslkey,
-  cert: sslcert
-};
-*/
-
 app.use(express.static('public'));
 
-// create the connection to database
+//yhteys tietokantaan
 const connection = database.connect();
-//testataan toimiiko tietokanta
 
-database.select(connection, (results) => {
-});
+///////////// Tietokannan lauseet
 
 const insertToDB = (data, res, next) => {
   database.insert(data, connection, () => {
+    next();
+  });
+};
+
+const tykkaa2update = (nimi, id, res, next) => {
+  database.tykkaa2update(nimi, id, connection, () => {
+    next();
+  });
+};
+
+const tykkaa2 = (data, res, next) => {
+  database.tykkaa2(data, connection, () => {
+    next();
+  });
+};
+
+const dislike2 = (nimi, res, next) => {
+  database.dislike2(nimi,connection, () => {
+    next();
+  });
+};
+
+const dislike2update = (nimi, id, res, next) => {
+  database.dislike2update(nimi, id, connection, () => {
+    next();
+  });
+};
+
+const insertView = (data, res, next) => {
+  database.insertView(data, connection, () => {
     next();
   });
 };
@@ -60,9 +76,9 @@ const report = (kuvaId, req, next) =>{
   })
 };
 
-//login eka vaihe  //tunnus, salasana, ip
-const login = (data1, data2, data3, res, next) => {
-  database.login(data1, data2, data3, connection, () => {
+//login eka vaihe  //tunnus, salasana, ip time
+const login = (data1, data2, data3, data4, res, next) => {
+  database.login(data1, data2, data3, data4, connection, () => {
     next();
   });
 };
@@ -99,6 +115,24 @@ const selectAll = (req, next) => {
   });
 };
 
+//tarkasta onko käyttäjä tykännyt kuvasta
+const tarkastaTykkays = (kayttaja_nimi,id,req, next) => {
+  database.tarkastaTykkays(kayttaja_nimi,id,connection, (results) => {
+    req.custom = results;
+    next();
+  });
+};
+
+
+//tarkasta onko käyttäjä tykännyt kuvasta
+const tarkastaDisTykkays = (kayttaja_nimi,id,req, next) => {
+  database.tarkastaDisTykkays(kayttaja_nimi,id,connection, (results) => {
+    req.custom = results;
+    next();
+  });
+};
+
+
 //logout
 const logout = (data,  res, next) => {
   database.logout(data,  connection, () => {
@@ -110,6 +144,43 @@ const logout = (data,  res, next) => {
 const haeTykkays = (data, req, next) => {
   console.log('haetykkäys');
   database.haeTykkays(data, connection, (results) => {
+  //  req.custom = results;
+    next();
+  });
+};
+
+const getUserId = (data, req, next) => {
+  console.log('hae käyttäjä id');
+  database.getUserId(data, connection, (results) => {
+    req.custom = results;
+    next();
+  });
+};
+
+const countViews = (data, req, next) => {
+  database.countViews(data, connection, (results) => {
+    req.custom = results;
+    next();
+  });
+};
+
+
+const countLikes = (data, req, next) => {
+  database.countLikes(data, connection, (results) => {
+    return results;
+    next();
+  });
+};
+
+const updateViews = (kuvaId, views, req, next) => {
+  database.updateViews(kuvaId, views, connection, (results) => {
+    next();
+  });
+};
+
+const checkUserView = (data, req, next) => {
+  console.log('katso onko käyttäjä jo antanut viewin');
+  database.checkUserView(data, connection, (results) => {
     req.custom = results;
     next();
   });
@@ -144,37 +215,15 @@ const dislike = (data, res, next) => {
 };
 
 
-
-const selectAll2 = (req, next) => {
-  database.insertA(connection, (results) => {
-    req.custom = results;
-    next();
-  });
-};
-
-//hae päivitetyt tiedot tietokannasta
+//hae päivitetyt tiedot tietokannasta kun sivu avataan
 app.use('/pageLoad', (req, res, next) => {
   selectAll(req, next);
 });
-
 
 //lähetä tiedot selaimeen//
 app.use('/pageLoad', (req, res) => {
   res.send(req.custom);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-//TARKASTA ETTÄ checkIp(req.ip, REQ, next); EIKÄ RES!!!!!!
 
 //tarkasta kirjautuneet käyttäjät
 app.use('/loggedUsers', (req, res, next) => {
@@ -182,11 +231,8 @@ app.use('/loggedUsers', (req, res, next) => {
 });
 
 app.use('/loggedUsers', (req, res,next) => {
-
   //katso kuka on onlinessa:
   for(let i=0; i<req.custom.length; i++) {
-
- //   console.log("USER: "+req.custom[i].kayttaja_nimi);
     checkIfLogged(req.custom[i].kayttaja_nimi, req.ip, req, next);
   }
   console.log("Req.custom: ",req.custom);
@@ -194,73 +240,92 @@ app.use('/loggedUsers', (req, res,next) => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//hae päivitetyt tiedot tietokannasta
+//hae kommentit
 app.use('/loadComments', (req, res, next) => {
- // console.log(req.body);
   const data = req.body.Id;
- // console.log(data);
   selectComments(data, req, next);
 });
-//lähetä tiedot selaimeen//
+//lähetä tiedot selaimeen
 app.use('/loadComments', (req, res) => {
- // console.log(req.custom);
+  console.log("loadcomment: ",req.custom);
   res.send(req.custom);
 });
-
-
-
 
 
 //Tykkays systeemi
+
+//tarkasta aluksi onko käyttäjä tykännyt kuvasta ennen
 app.post('/like', (req, res, next) => {//
-  console.log('body', req.body);
-  const data = req.body.kuvaId;
-  haeTykkays(data, req, next);
+tarkastaTykkays(req.body.kirjautunut,req.body.kuvaId,req,next);
 });
 
 app.use('/like', (req, res, next) => {
-  const data = req.body.kuvaId;
-  console.log("Tykkää", req.body);
-  tykkaa(data, req, next);
-  res.send(req.custom);
+  //jos ei ole aiemmin tykätty tästä kuvasta, niin lisätään uusi rivi tykkaykset tauluun
+  if(req.custom.length == 0){
+    const data = [req.body.kuvaId, req.body.kirjautunut, 0, 0];
+    tykkaa2(data, req, next);
+  }else{
+    console.log("On jo tykätty");
+    next();
+  }
 });
 
+//lisätään tykkäys sekä tykkaykset että kuvat tauluun
+app.use('/like', (req, res, next) => {
+  try{
+    if(req.custom[0].ttykkaa==0){//jos ei olla tykätty
+        const data = req.body.kuvaId;
+        tykkaa(req.body.kuvaId, req, next);
+        tykkaa2update(req.body.kirjautunut,req.body.kuvaId, req, next);
+        req.custom[0].tykkaa++; //lisätään yksi loppuun
+        req.custom[0].eitykkaa--;
+      }else{
+        console.log("On jo tykätty 2");
+      }
+    }catch{}
+    res.send(req.custom);
+});
+
+
+
 //Dislike systeemi
-app.post('/dislike', (req, res, next) => {//
- // console.log('body', req.body);
-  const data = req.body.kuvaId;
-  haeDisTykkays(data, req, next);
+
+app.post('/dislike', (req, res, next) => {
+tarkastaDisTykkays(req.body.kirjautunut,req.body.kuvaId,req,next);
 });
 
 app.use('/dislike', (req, res, next) => {
-  const data = req.body.kuvaId;
-  dislike(data, req, next);
+  //jos ei ole aiemmin tykätty tästä kuvasta, niin lisätään uusi rivi tykkaykset tauluun
+  if(req.custom.length == 0){
+      const data = [req.body.kuvaId, req.body.kirjautunut, 0, 0];
+      dislike2(data, req, next); //lisää uusi rivi kuvan kohdalle
+    }else{
+      console.log("On jo eitykätty");
+      next();
+    }
+});
+
+//lisätään dislike sekä tykkaykset että kuvat tauluun
+app.use('/dislike', (req, res, next) => {
+ try{//jos tykätään ekaa kertaa, niin ala_tykkaa ei ole olemmassa
+   if(req.custom[0].ala_tykkaa == 0){
+      const data = req.body.kuvaId;
+      dislike(req.body.kuvaId, req, next);
+      dislike2update(req.body.kirjautunut,req.body.kuvaId, req, next);
+      req.custom[0].tykkaa--; //lisätään yksi loppuun
+      req.custom[0].eitykkaa++;
+    }else{
+      console.log("On jo eitykätty 2");
+    }
+  }catch{}
   res.send(req.custom);
 });
 
-
-
-
-
-
 //kommentti systeemi
 app.post('/comment', (req, res, next) => {//
- // console.log('body', req.body);
-  const data = [req.body.kuvaId, 0, req.body.comment];
+  const data = [req.body.kuvaId, req.body.user , req.body.comment];
   addComment(data, req, next);
-
+  //lähetään selaimeen alert
   const json = {
     success: "Comment succesfully sent!"
   }
@@ -268,37 +333,25 @@ app.post('/comment', (req, res, next) => {//
 });
 
 
-
-
 //Tee käyttäjä
 app.post('/createAccount',  (req, res, next) => {
-  console.log(req.body);
   const data = [0,req.body.user,"random@com", req.body.pw, 0, null, null]; //ip alkuun
   insertUser(data, req, next);
-  console.log(req.custom);
   res.send(req.body);
 });
 
 
-
 //Kirjaudu sisään
 app.post('/login',  (req, res, next) => {
-  console.log("Login");
-  console.log(req.body);
-  login(req.body.logUser, req.body.logPw, req.ip, req, next);
+  login(req.body.logUser, req.body.logPw, req.ip, req.body.aika, req, next);
 });
 //lähetä fronttiin logged_in joko 1 tai 0
 app.use('/login',  (req, res, next) => {
-  console.log("ennen checklogin");
   const tunnus = req.body.logUser;
-  console.log(req.body);
-  console.log(req.body.logUser)
   checkLogin(tunnus,req,next);
 });
 
 app.use('/login',  (req, res, next) => {
-console.log("jälkeen checklogin");
-console.log("Req.custom: ",req.custom);
 res.send(req.custom);
 });
 
@@ -306,40 +359,68 @@ res.send(req.custom);
 
 //ulos loggaus
 app.post('/profileLogout',  (req, res, next) => {
-  console.log("Ulos loggaus");
   logout(req.body.user, req, next);
-  console.log(req.body);
+  //palataan etusivulle
   res.redirect('/');
 });
 
 
-//REPORT
-
+//Report
 app.post('/report', (req, res, next) =>{
-  console.log("REPORT");
-  console.log("KUVA jota reportetaaN: ", req.body);
-  const reportti = [req.body.report];
+  const reportti = [req.body.kuvaId];
   report(reportti, req, next);
   res.send(req.body);
-
 });
 
 
+//Viewi systeemi
+//katsotaan alkuun kuka käyttäjä katsoo kuvaa
+app.post('/view', (req, res, next) =>{
+  getUserId(req.body.user,req,next);
+});
 
+//seuraavaksi katsotaan onko hän jo katsonut kuvan kertaalleen
+app.use('/view', (req, res, next) =>{
+  try{
+    checkUserView(req.body.kuvaId, res, next);
+  }catch{
+    console.log("ei kirjautunut");
+  }
+});
 
+app.use('/view', (req, res, next) =>{
+  try{
+    const data = [ req.body.kuvaId, req.custom[0].kayttaja_id, null];
+    insertView(data, res, next);
+  }catch{}
+});
 
+//lasketaan yhden kuvan kaikki viewit
+app.use('/view', (req, res, next) =>{
+  countViews(req.body.kuvaId,req, next);
+});
 
+//päivitetään nämä kuvat tauluun
+app.use('/view', (req, res, next) =>{
+  updateViews(req.body.kuvaId,req.custom[0].viewCount,req, next);
+});
 
-//tallenna tiedosto
+app.use('/view', (req, res, next) =>{
+  console.log("final req.custom: ",req.custom[0].viewCount);
+  res.send(req.custom);
+});
+
+//Lataa kuva
 app.post('/upload', upload.single('kuva'), (req, res, next) => {
   console.log(req.body);
-  console.log(req.file);
+  console.log("aika:"+req.body.time);
+  console.log("file:",req.file);
   next();
 });
 
 // tallenna tiedot tietokantaan
 app.use('/upload', (req, res, next) => {
-  const data = [0, req.body.user, req.file.filename, 'text', 0, 0, 0,req.body.tag];
+  const data = [0, req.body.user, req.file.filename, 'text', 0, 0, 0,req.body.tag,req.body.time];
   insertToDB(data, res, next);
 });
 //hae päivitetyt tiedot tietokannasta
@@ -348,16 +429,8 @@ app.use('/upload', (req, res, next) => {
 });
 //lähetä tiedot selaimeen//
 app.use('/upload', (req, res) => {
-  console.log(req.custom); //kaikki kuvat
   res.send(req.custom);
 });
 
-/*
-app.get('/test', (req,res) => {
-  if (req.secure) res.send('https :)');
-  else res.send('hello not secure?');
-});
-*/
-
-app.listen(8000); //normal http traffic
-// https.createServer(options, app).listen(3000); //https traffic
+//avaa yhteys portissa 8000, localhost:8000
+app.listen(8000);
--- modules/database.js
+++ modules/database.js
@@ -1,16 +1,14 @@
 'use strict';
-// get the client
+
 const mysql = require('mysql2');
 
 const connect = () => {
 
-// create the connection to database
-
   const connection = mysql.createConnection({
-    host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'some'
+      host: 'localhost',
+  		user: 'root',
+  		password: '',
+  		database: 'some'
   });
   //Tarkastetaan saadaanko MySql yhteys
   connection.connect(function(error){
@@ -24,10 +22,10 @@ const connect = () => {
 };
 
 //Loggaa sisään jos pw ja username täsmäävät
-const login = (tunnus, salasana, ip, connection, callback) => {
+const login = (tunnus, salasana, ip, time, connection, callback) => {
 
   connection.execute(
-      "UPDATE kayttaja SET logged_in = CASE WHEN salasana ='"+salasana+"'  THEN 1 ELSE 0 END, ip = CASE WHEN salasana ='"+salasana+"' THEN '"+ip+"' ELSE NUll END WHERE kayttaja_nimi IN ('"+tunnus+"')",
+      "UPDATE kayttaja SET logged_in = CASE WHEN salasana ='"+salasana+"'  THEN 1 ELSE 0 END, ip = CASE WHEN salasana ='"+salasana+"' THEN '"+ip+"' ELSE NUll END, last_login = CASE WHEN salasana ='"+salasana+"' THEN '"+time+"' ELSE NUll END WHERE kayttaja_nimi IN ('"+tunnus+"')",
       (err, results, fields) => {
         callback();
       },
@@ -45,7 +43,7 @@ const checkLogin = (data, connection, callback) => {
   );
 };
 
-
+//tarkista mikä on käyttäjän IP osoite
 const checkIP = (ip,  connection, callback) => {
   console.log("IP:  "+ip);
   connection.execute(
@@ -56,19 +54,18 @@ const checkIP = (ip,  connection, callback) => {
   );
 };
 
+//valitse kaikki kuvat
 const select = (connection, callback) => {
-  // simple query
   connection.query(
       'SELECT * FROM kuvat;',
       (err, results, fields) => {
-        console.log(err);
         callback(results);
       },
   );
 };
 
 
-
+//tarkasta onko tietty käyttäjä tietystä ip:stä logannut sisään
 const checkIfLogged = (tunnus, ip,  connection, callback) => {//
   console.log("Tunnus: "+tunnus+" , "+ip);
   connection.execute(
@@ -91,51 +88,43 @@ const logout = (tunnus, connection, callback) => {
   );
 };
 
-
+//lisää uusi käyttäjä
 const insertUser = (data, connection, callback) => {
   console.log("Inser user")
   connection.execute(
       'INSERT INTO kayttaja (kayttaja_id, kayttaja_nimi, sahkoposti, salasana, logged_in, ip, last_login) VALUES (?, ?, ?, ?, ?, ?, ?);',
       data,
       (err, results, fields) => {
-        console.log(results); // results contains rows returned by server
-        console.log(err);
         callback();
       },
   );
   console.log("sql done");
 };
 
+const insertView = (data, connection, callback) => {
+  connection.execute(
+      'INSERT INTO views (kuva_id, kayttaja_id, ei_kirjautunut_kayttaja) VALUES (?, ?, ?);',
+      data,
+      (err, results, fields) => {
+        console.log(err);
+        callback();
+      },
+  );
+};
 
+//lisää uusi kuva
 const insert = (data, connection, callback) => {
-
   connection.execute(
-      'INSERT INTO kuvat (kuva_id, kayttaja_nimi, URL, kuva_teksti, views, tykkaa, eitykkaa,tag) VALUES (?, ?, ?, ?, ?,?,?,?);',
+      'INSERT INTO kuvat (kuva_id, kayttaja_nimi, URL, kuva_teksti, views, tykkaa, eitykkaa,tag,upload_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);',
       data,
       (err, results, fields) => {
-        console.log(results); // results contains rows returned by server
-        // console.log(fields); // fields contains extra meta data about results, if available
-        console.log(err);
+        console.log("insert results:",results);
         callback();
       },
   );
 };
 
-const insertA = (data, connection, callback) => {
-  console.log("uppaus2?");
-  // simple query
-  connection.execute(
-      'INSERT INTO wp_users (ufname, ulname, ufile, uthumb, mimetype, coordinates) VALUES (?, ?, ?, ?, ?, ?);',
-      data,
-      (err, results, fields) => {
-       // console.log(results); // results contains rows returned by server
-        // console.log(fields); // fields contains extra meta data about results, if available
-        console.log(err);
-        callback();
-      },
-  );
-};
-
+//etsi tykkäys kuva id:n perusteella
 const haeTykkays = (data, connection, callback) => {
   // simple query
   connection.query(
@@ -146,18 +135,63 @@ const haeTykkays = (data, connection, callback) => {
   );
 };
 
-
+//lisää kuvaan tykkäys ja ota pois dislike
 const tykkaa = (id, connection, callback) => {
   // simple query
   connection.execute(
-      'UPDATE kuvat SET tykkaa = tykkaa + 1 WHERE kuva_id = "'+id+'"',
+      'UPDATE kuvat SET tykkaa = tykkaa + 1,eitykkaa = eitykkaa - 1 WHERE kuva_id = "'+id+'"',
       (err, results, fields) => {
-      //  console.log(results);
         callback();
       },
   );
 };
 
+//tarkasta onko käyttäjä tykännyt aiemmin tästä kuvasta
+const tarkastaTykkays = (kayttaja_nimi,id, connection, callback) => {
+    console.log("kayttaja_nimi: "+kayttaja_nimi+" , id:"+id);
+  connection.execute(
+    "SELECT tykkaykset.tykkaa AS ttykkaa, tykkaykset.ala_tykkaa AS ala_tykkaa, kuvat.kuva_id, kuvat.tykkaa, kuvat.eitykkaa FROM tykkaykset INNER JOIN kuvat ON tykkaykset.kuva_id = kuvat.kuva_id AND tykkaykset.kayttaja_nimi = '"+kayttaja_nimi+"'AND tykkaykset.kuva_id = "+id+" ",
+      (err, results, fields) => {
+        callback(results);
+      },
+  );
+};
+
+//lisää tykkaykset tauluun uusi rivi käyttäjästä ja mistä kuvasta hän tykkää
+const tykkaa2 = (data, connection, callback) => {
+  connection.execute(
+      'INSERT INTO tykkaykset (kuva_id, kayttaja_nimi, tykkaa, ala_tykkaa) VALUES (?, ?, ?, ?);',
+      data,
+      (err, results, fields) => {
+        callback();
+      },
+  );
+};
+
+//tarkasta onko käyttäjä dislikennut tästä kuvasta aiemmin
+const tarkastaDisTykkays = (kayttaja_nimi, id, connection, callback) => {
+  connection.execute(
+    "SELECT tykkaykset.tykkaa AS ttykkaa, tykkaykset.ala_tykkaa AS ala_tykkaa, kuvat.kuva_id, kuvat.tykkaa , kuvat.eitykkaa FROM tykkaykset INNER JOIN kuvat ON tykkaykset.kuva_id = kuvat.kuva_id AND tykkaykset.kayttaja_nimi = '"+kayttaja_nimi+"'AND tykkaykset.kuva_id = "+id+" ",
+      (err, results, fields) => {
+        callback(results);
+      },
+  );
+};
+
+//lisää tykkaykset tauluun uusi rivi käyttäjästä ja mistä kuvasta hän ei tykkää
+const dislike2 = (data, connection, callback) => {
+  console.log("data: "+data);
+  connection.execute(
+      'INSERT INTO tykkaykset (kuva_id, kayttaja_nimi, tykkaa, ala_tykkaa) VALUES (?, ?, ?, ?);',
+      data,
+      (err, results, fields) => {
+        console.log(err);
+        callback();
+      },
+  );
+};
+
+//etsi tietyn kuvan disliket
 const haeDisTykkays = (data, connection, callback) => {
   connection.query(
       'SELECT * FROM kuvat WHERE kuva_id = "'+data+'";',
@@ -168,55 +202,113 @@ const haeDisTykkays = (data, connection, callback) => {
   );
 };
 
-
-const dislike = (data, connection, callback) => {
-  // simple query
-  connection.execute(
-      'UPDATE kuvat SET eitykkaa = eitykkaa + 1 WHERE kuva_id = "'+data+'"',
-      (err, results, fields) => {
-        callback();
-      },
-  );
-};
-
-const addComment = (data, connection, callback) => {
-  console.log("add comment: "+data);
-  connection.execute(
-      'INSERT INTO kommentit (kuva_id, kayttaja_id, kommentti) VALUES (?, ?, ?);',
-      data,
-      (err, results, fields) => {
-        console.log(err);
-        callback();
-      },
-  );
-};
-
-const selectComments = (data,connection, callback) => {
- // console.log("data: "+data);
+//hae käyttäjän id nimen perusteella
+const getUserId = (data, connection, callback) => {
   connection.query(
-      'SELECT kommentti, kuva_id FROM kommentit WHERE kuva_id = '+data+';',
+      'SELECT kayttaja_id FROM kayttaja WHERE kayttaja_nimi = "'+data+'";',
       (err, results, fields) => {
-        console.log(err);
         callback(results);
       },
   );
 };
 
-const insertTag = (data, connection, callback) => {
+//lisää kuvaan 1 dislike ja ota poist 1 tykkäys
+const dislike = (data, connection, callback) => {
+  connection.execute(
+      'UPDATE kuvat SET eitykkaa = eitykkaa + 1, tykkaa = tykkaa - 1 WHERE kuva_id = "'+data+'"',
+      (err, results, fields) => {
+        callback();
+      },
+  );
+};
 
+//päivitä tykkaykset taulun tykkäys dislikeen
+const dislike2update = (data, data2, connection, callback) => {
+  connection.execute(
+      'UPDATE tykkaykset SET tykkaa = 0, ala_tykkaa = 1 WHERE kayttaja_nimi = "'+data+'" AND kuva_id = "'+data2+'"',
+      (err, results, fields) => {
+        callback();
+      },
+  );
+};
+
+//päivitä tykkaykset taulun dislike likeen
+const tykkaa2update = (data, data2, connection, callback) => {
+  // simple query
+  connection.execute(
+      'UPDATE tykkaykset SET tykkaa = 1, ala_tykkaa = 0 WHERE kayttaja_nimi = "'+data+'" AND kuva_id = "'+data2+'"',
+      (err, results, fields) => {
+        callback();
+      },
+  );
+};
+
+//lisää kommentti tiettyyn kuvaan tietyltä käyttäjältä
+const addComment = (data, connection, callback) => {
+  connection.execute(
+      'INSERT INTO kommentit (kuva_id, kayttaja_nimi, kommentti) VALUES (?, ?, ?);',
+      data,
+      (err, results, fields) => {
+        callback();
+      },
+  );
+};
+
+//valise yhden kuvan kommentit
+const selectComments = (data,connection, callback) => {
+  connection.query(
+      'SELECT kommentti, kuva_id, kayttaja_nimi FROM kommentit WHERE kuva_id = '+data+';',
+      (err, results, fields) => {
+        callback(results);
+      },
+  );
+};
+
+//tarkasta onko käyttäjä katsonut kuvan
+const checkUserView = (data, connection, callback) => {
+  console.log("kuva id : "+data);
+  connection.query(
+      'SELECT kayttaja_id FROM views WHERE kuva_id = "'+data+'";',
+      (err, results, fields) => {
+        callback(results);
+      },
+  );
+};
+
+//laske yhden kuvan kaikki viewit
+const countViews = (data,connection, callback) => {
+  connection.query(
+      'SELECT COUNT(kuva_id) AS viewCount FROM views WHERE kuva_id = '+data+';',
+      (err, results, fields) => {
+        callback(results);
+      },
+  );
+};
+
+//laske yhden kuvan kaikki viewit
+const updateViews = (kuvaId, views ,connection, callback) => {
+  connection.query(
+      'UPDATE kuvat SET views = '+views+' WHERE kuva_id = '+kuvaId+';',
+      (err, results, fields) => {
+        callback();
+      },
+  );
+};
+
+//lisää tagi
+const insertTag = (data, connection, callback) => {
   connection.execute(
       'INSERT INTO tags (?,?);',
       data,
       (err, results, fields) => {
-    console.log(results);
-  console.log(err);
-  callback();
-},
-);
+        callback();
+      },
+    );
 };
 
-//DATABASE
+//Reporttaa tietty kuva
 const reportImage= (kuvaId, connection, callback)=> {
+  console.log("kuva id: "+kuvaId);
   connection.execute(
       'update kuvat set reported = 1 where kuva_id = '+kuvaId+';',
       (err, results) => {
@@ -226,16 +318,30 @@ const reportImage= (kuvaId, connection, callback)=> {
   );
 };
 
+//laske yhden kuvan kaikki viewit
+const countLikes = (data,connection, callback) => {
+  connection.query(
+      'SELECT COUNT(tykkaa) AS likeCount FROM tykkaykset WHERE kuva_id = '+data+';',
+      (err, results, fields) => {
+        callback(results);
+      },
+  );
+};
+
+
 
 module.exports = {
   connect: connect,
   select: select,
   insert: insert,
-  insertA: insertA,
   haeTykkays: haeTykkays,
   tykkaa: tykkaa,
+  tykkaa2: tykkaa2,
+  tykkaa2update: tykkaa2update,
   haeDisTykkays: haeDisTykkays,
   dislike: dislike,
+  dislike2: dislike2,
+  dislike2update: dislike2update,
   addComment: addComment,
   selectComments: selectComments,
   insertUser: insertUser,
@@ -245,4 +351,12 @@ module.exports = {
   checkIfLogged: checkIfLogged,
   checkIP: checkIP,
   reportImage: reportImage,
+  insertView: insertView,
+  getUserId: getUserId,
+  checkUserView: checkUserView,
+  countViews: countViews,
+  countLikes: countLikes,
+  updateViews: updateViews,
+  tarkastaTykkays: tarkastaTykkays,
+  tarkastaDisTykkays: tarkastaDisTykkays,
 };
